360faafc0c53c1ab4cf7d7c2ef117944e2ef19d1
delvedor <tommydelved@gmail.com>
2016-05-20 11:12:25 +0200

    Added test and bench

---
+++ bench.js
@@ -0,0 +1,59 @@
+'use strict'
+
+const bench = require('fastbench')
+const tyval = require('./tyval')
+
+const run = bench([
+  function isString (done) {
+    process.nextTick(() => { if (tyval('', 1).isString()) done() })
+  },
+  function isNumber (done) {
+    process.nextTick(() => { if (tyval(1, 1).isNumber()) done() })
+  },
+  function isNull (done) {
+    process.nextTick(() => { if (tyval(null, 1).isNull()) done() })
+  },
+  function isUndefined (done) {
+    process.nextTick(() => { if (tyval(undefined, 1).isUndefined()) done() })
+  },
+  function isBoolean (done) {
+    process.nextTick(() => { if (tyval(true, 1).isBoolean()) done() })
+  },
+  function isObject (done) {
+    process.nextTick(() => { if (tyval({}, 1).isObject()) done() })
+  },
+  function isFunction (done) {
+    process.nextTick(() => { if (tyval(function () {}, 1).isFunction()) done() })
+  },
+  function max (done) {
+    process.nextTick(() => { if (tyval(5, 1).max(10)) done() })
+  },
+  function min (done) {
+    process.nextTick(() => { if (tyval(5, 1).min(1)) done() })
+  },
+  function positive (done) {
+    process.nextTick(() => { if (tyval(5, 1).positive()) done() })
+  },
+  function negative (done) {
+    process.nextTick(() => { if (tyval(-5, 1).negative()) done() })
+  },
+  function integer (done) {
+    process.nextTick(() => { if (tyval(5, 1).integer()) done() })
+  },
+  function float (done) {
+    process.nextTick(() => { if (tyval(5.1, 1).float()) done() })
+  },
+  function safeInteger (done) {
+    process.nextTick(() => { if (tyval(5, 1).safeInteger()) done() })
+  },
+  function finite (done) {
+    process.nextTick(() => { if (tyval(5, 1).finite()) done() })
+  },
+  function numberValidation (done) {
+    process.nextTick(() => {
+      if (tyval(1, 6).isNumber().min(-5).max(10).integer().finite().safeInteger()) done()
+    })
+  }
+], 1000)
+
+run(run)
---
+++ test.js
@@ -0,0 +1,123 @@
+'use strict'
+
+const test = require('ava')
+const tyval = require('./tyval')
+
+test('$ setter', (t) => {
+  t.plan(6)
+  t.is(tyval('').variable, '')
+  t.is(tyval(5).variable, 5)
+  t.is(tyval(null).variable, null)
+  t.is(tyval(true).variable, true)
+  t.is(tyval(false).variable, false)
+  t.deepEqual(tyval({ a: 1 }).variable, { a: 1 })
+})
+
+test('string', (t) => {
+  t.plan(2)
+  t.true(tyval('', 1).isString())
+  t.false(tyval(1, 1).isString())
+})
+
+test('number', (t) => {
+  t.plan(2)
+  t.true(tyval(1, 1).isNumber())
+  t.false(tyval('', 1).isNumber())
+})
+
+test('null', (t) => {
+  t.plan(2)
+  t.true(tyval(null, 1).isNull())
+  t.false(tyval(1, 1).isNull())
+})
+
+test('undefined', (t) => {
+  t.plan(2)
+  t.true(tyval(undefined, 1).isUndefined())
+  t.false(tyval(1, 1).isUndefined())
+})
+
+test('boolean', (t) => {
+  t.plan(2)
+  t.true(tyval(true, 1).isBoolean())
+  t.false(tyval(1, 1).isBoolean())
+})
+
+test('object', (t) => {
+  t.plan(2)
+  t.true(tyval({}, 1).isObject())
+  t.false(tyval(1, 1).isObject())
+})
+
+test('function', (t) => {
+  t.plan(2)
+  t.true(tyval(function () {}, 1).isFunction())
+  t.false(tyval(1, 1).isFunction())
+})
+
+test('max', (t) => {
+  t.plan(3)
+  t.true(tyval(5, 2).isNumber().max(10))
+  t.false(tyval(5, 2).isNumber().max(4))
+  t.true(tyval('test', 2).isString().max(5))
+})
+
+test('min', (t) => {
+  t.plan(3)
+  t.true(tyval(15, 2).isNumber().min(10))
+  t.false(tyval(3, 2).isNumber().min(4))
+  t.true(tyval('test', 2).isString().min(2))
+})
+
+test('positive', (t) => {
+  t.plan(2)
+  t.true(tyval(1, 1).positive())
+  t.false(tyval(-1, 1).positive())
+})
+
+test('negative', (t) => {
+  t.plan(2)
+  t.true(tyval(-1, 1).negative())
+  t.false(tyval(1, 1).negative())
+})
+
+test('integer', (t) => {
+  t.plan(2)
+  t.true(tyval(1, 1).integer())
+  t.false(tyval(1.5, 1).integer())
+})
+
+test('float', (t) => {
+  t.plan(2)
+  t.true(tyval(1.1, 1).float())
+  t.false(tyval(1, 1).float())
+})
+
+test('safeInteger', (t) => {
+  t.plan(2)
+  t.true(tyval(1, 1).safeInteger())
+  t.false(tyval(Number.MAX_SAFE_INTEGER + 1, 1).safeInteger())
+})
+
+test('finite', (t) => {
+  t.plan(2)
+  t.true(tyval(1, 1).finite())
+  t.false(tyval(Number.POSITIVE_INFINITY, 1).finite())
+})
+
+test('test number validation', (t) => {
+  t.plan(2)
+  t.true(tyval(1, 6).isNumber().min(-5).max(10).integer().finite().safeInteger())
+  t.false(tyval(1, 6).isNull().min(0).max(5).integer().finite().safeInteger())
+})
+
+test('extend', (t) => {
+  tyval().extend(function isZero () {
+    this.check &= this.variable === 0
+    return this.plan()
+  })
+  t.plan(3)
+  t.is(typeof tyval().isZero, 'function')
+  t.true(tyval(0, 2).isNumber().isZero())
+  t.false(tyval(1, 2).isNumber().isZero())
+})
