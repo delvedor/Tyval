0074e0a593ab62eece33721b47e230200930cbb8
delvedor <tommydelved@gmail.com>
2016-06-13 09:39:17 +0200

    Added test runner

---
+++ test.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env zsh
+
+# This script run test and bench under Node v4, v5 and v6.
+# By default it runs only the test, if you pass '--bench' it will run the benchmark as well.
+
+set -e
+
+. ~/.nvm/nvm.sh
+
+bench=${1:-default}
+
+echo "\n-------------------------"
+echo "| Test under Node.js v4 |"
+echo "-------------------------"
+nvm use "4.4.5"
+npm test
+if [ $bench == "--bench" ]
+then
+  npm run bench
+fi
+
+echo "\n-------------------------"
+echo "| Test under Node.js v5 |"
+echo "-------------------------"
+nvm use "5.11.1"
+npm test
+if [ $bench  == "--bench" ]
+then
+  npm run bench
+fi
+
+echo "\n-------------------------"
+echo "| Test under Node.js v6 |"
+echo "-------------------------"
+nvm use "6.2.1"
+npm test
+if [ $bench  == "--bench" ]
+then
+  npm run bench
+fi
+
+echo "\n--------------------"
+echo "| All test passed! |"
+echo "--------------------"
