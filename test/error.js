eecb74c36a516a0bccd953b7707f6959cd43016f
delvedor <tommydelved@gmail.com>
2016-08-21 11:45:57 +0200

    Updated test

---
+++ test/error.js
@@ -0,0 +1,59 @@
+'use strict'
+
+const tap = require('tap')
+const test = tap.test
+const tyval = require('../tyval')
+
+test('error', (t) => {
+  t.plan(4)
+  t.is(typeof tyval.error, 'function')
+  let err = tyval.error().toFunction()
+  t.is(typeof err, 'function')
+  t.true(err(new Error()))
+  t.false(err([]))
+})
+
+test('error.RangeError', (t) => {
+  t.plan(4)
+  t.is(typeof tyval.error.RangeError, 'function')
+  let err = tyval.error().RangeError().toFunction()
+  t.is(typeof err, 'function')
+  t.true(err(new RangeError()))
+  t.false(err(new Error()))
+})
+
+test('error.ReferenceError', (t) => {
+  t.plan(4)
+  t.is(typeof tyval.error.ReferenceError, 'function')
+  let err = tyval.error().ReferenceError().toFunction()
+  t.is(typeof err, 'function')
+  t.true(err(new ReferenceError()))
+  t.false(err(new Error()))
+})
+
+test('error.SyntaxError', (t) => {
+  t.plan(4)
+  t.is(typeof tyval.error.SyntaxError, 'function')
+  let err = tyval.error().SyntaxError().toFunction()
+  t.is(typeof err, 'function')
+  t.true(err(new SyntaxError()))
+  t.false(err(new Error()))
+})
+
+test('error.TypeError', (t) => {
+  t.plan(4)
+  t.is(typeof tyval.error.TypeError, 'function')
+  let err = tyval.error().TypeError().toFunction()
+  t.is(typeof err, 'function')
+  t.true(err(new TypeError()))
+  t.false(err(new Error()))
+})
+
+test('error.message', (t) => {
+  t.plan(4)
+  t.is(typeof tyval.error.message, 'function')
+  let err = tyval.error().message('some error').toFunction()
+  t.is(typeof err, 'function')
+  t.true(err(new Error('some error')))
+  t.false(err(new Error('other error')))
+})
